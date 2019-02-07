2ffb5cfa64385e9a463d7bc195b5890e49f24558
mooo <moohh91@gmail.com>
2019-02-06 23:14:06 -0500

    .

---
+++ bunny.heart/heartdirectives.js
@@ -0,0 +1,45 @@
+let whisper = require('./whisper');
+let secrets = require('./secrets');
+
+let aorta = require('../bunny.circulation/aorta');
+let venacava = require('../bunny.circulation/venacava');
+
+let dispatch = (context) => {
+    VEIN_ACTIONS[context.action](context);
+};
+
+let VEIN_ACTIONS = {
+    pong: (context) => {
+        whisper('pong received from ' + context.details.designator);
+        aorta.pump(DIRECTIVES['pong'](context));
+    },
+    route: (context) => {
+        whisper('route message received');
+        aorta.pump(DIRECTIVES['route'](context));
+    }
+};
+
+let DIRECTIVES = {
+    pong: (context) => {
+        return {
+            name: context.details.designator + '_artery',
+            designator: secrets.me,
+            designee: context.details.designator,
+            directive: {
+                type: 'pong'
+            }
+        };
+    },
+    route: (context) => {
+        let blood = context.details;
+        let routedirective = blood.directive.routedirective;
+
+        whisper('routing to target q: ' + routedirective.name);
+
+        return routedirective;
+    }
+};
+
+module.exports = {
+    dispatch
+};
