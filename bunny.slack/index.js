794100246176197a29f25fa61e305d9cb0ce679e
moohh <mo051191@yahoo.com>
2019-12-12 18:16:53 -0500

    .

--- bunny.slack/index.js
+++ bunny.slack/index.js
@@ -5,3 +5,28 @@ let secretsRepository = require('./secretsRepository');
 let configs = require('./configs');
 
 
+let { createServer } = require('http');
+let express =  require('express');
+let bodyParser = require('body-parser');
+let { createEventAdapter } = require('@slack/events-api');
+let slackSigningSecret = '';
+
+let port = 3010;
+
+let slackEvents = createEventAdapter(slackSigningSecret);
+
+let app = express();
+
+app.use('/slack', slackEvents.requestListener());
+
+app.post('/slacki', (req, res) => {
+    console.log(req.body);
+    res.json('world');
+});
+
+app.use(bodyParser());
+
+let server = createServer(app);
+server.listen(port, () => {
+    console.log(`Listening on port ${server.address().port}`);
+});
